2d4db9ed847ec10f621a5cb40277538ce1bef725
Justin Matthews <jnm212@ex.ac.uk>
2026-07-31 11:34:20 +0100

    some changes to eliminate R CHECK Notes

--- R/visATC-package.R
+++ R/visATC-package.R
@@ -1,6 +1,17 @@
 #' @keywords internal
 "_PACKAGE"
 
+utils::globalVariables("ATCdata")
+
+# to eliminate check NOTE
+# see R packages 11.4.1.1
+ignore_unused_imports <- function() {
+  igraph::add.edges()
+  tidyr::all_of()
+  graphlayouts::annotate_circle()
+  plotly::add_annotations()
+}
+
 #' @importFrom dplyr mutate
 #' @importFrom dplyr filter
 #' @importFrom dplyr bind_rows
@@ -12,5 +23,9 @@
 #' @importFrom dplyr if_else
 #' @importFrom dplyr case_when
 #' @importFrom methods show
+#' @importFrom methods new
+#' @importFrom utils data
+#' @importFrom rlang .data
+#' 
 ## usethis namespace: end
 NULL
